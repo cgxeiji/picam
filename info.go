348efa69fb39797b7f63e3a0b261e8371a50d217
Eiji Onchi <onchi.eiji@gmail.com>
2020-08-11 12:40:24 +0900

    Improved documentation

--- info.go
+++ info.go
@@ -1,3 +1,22 @@
-// Package picam is a Go API that uses raspiyuv that continuously reads the
-// latests frame from a Raspberry Pi Camera.
+// Package picam is a Go wrapper to `raspiyuv` to get `[]uint8` and `image.Image` data of
+// the latests frame captured by the Raspberry Pi camera.
+//
+// Under the hood, it executes:
+//	$ raspiyuv --timeout 0 --timelapse 0
+// to get raw frames.
+//
+// Currently, three image formats are available:
+//	* picam.YUV
+//	* picam.RGB
+//	* picam.Gray
+//
+// The time between frames, measured on a Raspberry Pi Zero W, is between `180ms` to
+// `210ms` for a `640x480` pixels image.
+//
+// If you want to test the speed in your system, run:
+//	$ cd $(go env GOPATH)/src/github.com/cgxeiji/picam
+//	$ go test -bench . -benchtime=10x
+//
+// This will take 10 frames and output the average time between each frame. Change
+// `-benchtime=10x` to `100x` or `Nx` to change the number of frames to test.
 package picam
